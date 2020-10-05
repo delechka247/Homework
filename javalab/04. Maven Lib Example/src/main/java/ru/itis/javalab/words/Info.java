b43be7cdf8fdfe71faaafa9586adf92e72238d4e
Adelya <delechka247@gmail.com>
2020-10-05 15:44:27 +0300

    q

---
+++ javalab/04. Maven Lib Example/src/main/java/ru/itis/javalab/words/Info.java
@@ -0,0 +1,19 @@
+package ru.itis.javalab.words;
+
+public class Info {
+    private int num;
+    private String site;
+
+    public Info(int num, String site) {
+        this.num = num;
+        this.site = site;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getSite() {
+        return site;
+    }
+}
