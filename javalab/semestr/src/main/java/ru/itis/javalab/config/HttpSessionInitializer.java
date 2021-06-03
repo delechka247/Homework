611979eacc0472f6e1ebb796bb7a3ef5ca0b06e0
Adelya <delechka247@gmail.com>
2021-06-03 16:23:48 +0300

    -

--- javalab/semestr/src/main/java/ru/itis/javalab/config/HttpSessionInitializer.java
+++
@@ -1,7 +0,0 @@
-package ru.itis.javalab.config;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class HttpSessionInitializer extends AbstractHttpSessionApplicationInitializer {
-}
-
--- javalab/semestr/src/main/java/ru/itis/javalab/utils/MailsGenerator.java
+++
@@ -1,5 +0,0 @@
-package ru.itis.javalab.utils;
-
-public interface MailsGenerator {
-    String getMailForConfirm(String serverUrl, String code);
-}
